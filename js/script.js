80259daf98dbe79a2835880369b762cf1cabc412
Daniel <danieljmnz15@hotmail.com>
2023-03-09 20:01:25 -0300

    cambio url de local a remoto para pdf

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 function download() {
     const downloadInstancia=document.createElement('a');
-    downloadInstancia.href='http://127.0.0.1:5502/sodapdf-converted.pdf'
+    downloadInstancia.href='https://danieljimenezl.netlify.app/sodapdf-converted.pdf'
     downloadInstancia.target='_blank';
     downloadInstancia.download='CV DanielJimenez'
     document.body.appendChild(downloadInstancia)
