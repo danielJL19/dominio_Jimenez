e55f108690953bb09e2246b73049cc3ead93e44c
Daniel <danieljmnz15@hotmail.com>
2023-07-29 22:30:50 -0400

    subida de nuevo pdf

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 function download() {
     const downloadInstancia=document.createElement('a');
-    downloadInstancia.href='https://danieljimenezl.netlify.app/sodapdf-converted.pdf'
+    downloadInstancia.href='https://danieljimenezl.netlify.app/cv2024.pdf'
     downloadInstancia.target='_blank';
     downloadInstancia.download='CV DanielJimenez'
     document.body.appendChild(downloadInstancia)
